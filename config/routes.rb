6521dcba26b06c5d67f153c36baa05aa4ce830a4
Uriel Juliatti Valle <uriel.juliattivalle@gmail.com>
2023-02-04 13:37:28 -0300

    Adicionando versionamento

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+require 'api_constraints'
+Rails.application.routes.draw do
+  namespace :api, defaults: { format: :json } do
+    scope module: :v1, 
+          constraints: ApiConstraints.new(version: 1, default: false) do
+      resources :articles
+    end  
+    
+    scope module: :v2, 
+          constraints: ApiConstraints.new(version: 2, default: true) do
+      resources :articles
+    end  
+  end
+end
