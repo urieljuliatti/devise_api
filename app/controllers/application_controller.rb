1c244c70987f605b6a882a5a7b6a52258f47cc73
Uriel Juliatti Valle <uriel.juliattivalle@gmail.com>
2023-02-05 09:00:05 -0300

    Add admin and nickname to update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::API
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :nickname, :admin])
   end
 end
