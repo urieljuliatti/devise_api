4fa8f8c9058260562420117eeb7f1fa9faa7aecf
Uriel Juliatti Valle <uriel.juliattivalle@gmail.com>
2023-02-05 10:05:27 -0300

    Add admin to devise_parameter_sanitizer sign_up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,6 @@ class ApplicationController < ActionController::API
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :nickname, :admin])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:admin])
   end
 end
