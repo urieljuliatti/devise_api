ff00c50551b49af08f0e3b04b631fda6a62fb664
Uriel Juliatti Valle <uriel.juliattivalle@gmail.com>
2023-02-06 10:48:31 -0300

    Changing to routing helper

--- spec/requests/registrations_spec.rb
+++ spec/requests/registrations_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe 'Registrations', type: :request do
   describe 'POST /create' do
     it 'creates a new User' do
       expect do
-        post '/api/auth', params: valid_attributes
+        post api_user_registration_path, params: valid_attributes
       end.to change(User, :count).by(1)
     end
   end
@@ -19,7 +19,7 @@ RSpec.describe 'Registrations', type: :request do
     let(:new_name) { Faker::Name.name }
 
     it 'updates the user' do
-      put '/api/auth',
+      put api_user_registration_path,
           params: { name: new_name }, headers: user.create_new_auth_token, as: :json
       user.reload
       expect(user.name).to eq(new_name)
@@ -32,7 +32,7 @@ RSpec.describe 'Registrations', type: :request do
     it 'destroys the user' do
       user
       expect do
-        delete '/api/auth', headers: user.create_new_auth_token, as: :json
+        delete api_user_registration_path, headers: user.create_new_auth_token, as: :json
       end.to change(User, :count).by(-1)
     end
   end
