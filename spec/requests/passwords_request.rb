213f1ba2c7d1bcfbdfe8eb05ea9c044ce723800b
Uriel Juliatti Valle <uriel.juliattivalle@gmail.com>
2023-02-06 10:37:54 -0300

    Add password reset specs

---
+++ spec/requests/passwords_request.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Passwords', type: :request do
+  let(:user) { create :user }
+
+  describe 'POST /create' do
+    it 'resets password' do
+      post '/api/auth/password', params: {
+        email: user.email,
+        redirect_url: api_user_password_url
+      }
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe 'PUT /update' do
+    it 'updates password' do
+      put '/api/auth/password', params: {
+        current_password: user.password,
+        password: 'newpassword',
+        password_confirmation: 'newpassword'
+      }, headers: user.create_new_auth_token, as: :json
+      user.reload
+      expect(user.valid_password?('newpassword')).to be_truthy
+      expect(response).to have_http_status(:ok)
+      expect(response.content_type).to eq('application/json; charset=utf-8')
+    end
+  end
+end
