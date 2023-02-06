d992bfe4a6f8ced177802508c50a52613a90cd7c
Uriel Juliatti Valle <uriel.juliattivalle@gmail.com>
2023-02-06 15:04:27 -0300

    Add another user to specs

--- spec/requests/v2/articles_spec.rb
+++ spec/requests/v2/articles_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 # rubocop: disable Metrics/BlockLength
 RSpec.describe '/articles', type: :request do
   let(:user) { create :user }
-  let(:another_user) { create :user}
+  let(:another_user) { create :user }
   let(:article) { create :article, user: user }
   let(:article_two) { create :article, user: another_user }
 
@@ -35,6 +35,13 @@ RSpec.describe '/articles', type: :request do
       expect(response).to be_successful
     end
 
+    it 'renders an article from a distinct user' do
+      article_two
+
+      get api_article_url(article_two), headers: another_user_valid_headers, as: :json
+      expect(response).to be_successful
+    end
+
     it_behaves_like 'user not logged in' do
       let(:url) do
         get api_article_url(article), headers: {}, as: :json
@@ -44,22 +51,42 @@ RSpec.describe '/articles', type: :request do
 
   describe 'POST /create' do
     context 'with valid parameters' do
-      it 'creates a new Article' do
-        expect do
-          post api_articles_url,
-               params: { article: valid_attributes },
-               headers: valid_headers,
-               as: :json
-        end.to change(Article, :count).by(1)
-      end
+      context 'with the first user' do 
+        it 'creates a new Article' do
+          expect do
+            post api_articles_url,
+              params: { article: valid_attributes },
+              headers: valid_headers,
+              as: :json
+          end.to change(Article, :count).by(1)
+        end
 
-      it 'renders a JSON response with the new article' do
-        post api_articles_url,
-             params: { article: valid_attributes },
-             headers: valid_headers,
-             as: :json
-        expect(response).to have_http_status(:created)
-        expect(response.content_type).to match(a_string_including('application/json'))
+        it 'renders a JSON response with the new article' do
+          post api_articles_url,
+            params: { article: valid_attributes },
+            headers: valid_headers,
+            as: :json
+          expect(response).to have_http_status(:created)
+          expect(response.content_type).to match(a_string_including('application/json'))
+        end
+      end
+      context 'with another user' do
+        it 'creates a new Article' do
+          expect do
+            post api_articles_url,
+              params: { article: valid_attributes },
+              headers: another_user_valid_headers,
+              as: :json
+          end.to change(Article, :count).by(1)
+        end
+        it 'renders a JSON response with the new article' do
+          post api_articles_url,
+            params: { article: valid_attributes },
+            headers: another_user_valid_headers,
+            as: :json
+          expect(response).to have_http_status(:created)
+          expect(response.content_type).to match(a_string_including('application/json'))
+        end
       end
     end
 
@@ -95,24 +122,38 @@ RSpec.describe '/articles', type: :request do
 
   describe 'PATCH /update' do
     context 'with valid parameters' do
-      let(:new_attributes) { attributes_for :article }
+      context 'with the first user' do
+        let(:new_attributes) { attributes_for :article }
 
-      it 'updates the requested article' do
-        patch api_article_url(article),
-              params: { article: new_attributes },
-              headers: valid_headers,
-              as: :json
-        article.reload
-        expect(article.title).to eq(new_attributes[:title])
+        it 'updates the requested article' do
+          patch api_article_url(article),
+                params: { article: new_attributes },
+                headers: valid_headers,
+                as: :json
+          article.reload
+          expect(article.title).to eq(new_attributes[:title])
+        end
+
+        it 'renders a JSON response with the article' do
+          patch api_article_url(article),
+                params: { article: new_attributes },
+                headers: valid_headers,
+                as: :json
+          expect(response).to have_http_status(:ok)
+          expect(response.content_type).to match(a_string_including('application/json'))
+        end
       end
-
-      it 'renders a JSON response with the article' do
-        patch api_article_url(article),
-              params: { article: new_attributes },
-              headers: valid_headers,
-              as: :json
-        expect(response).to have_http_status(:ok)
-        expect(response.content_type).to match(a_string_including('application/json'))
+      context 'with another user' do
+        let(:another_article) { create :article, user: another_user }
+        let(:another_user_article_new_attributes) { { title: 'Another' } }
+        it 'updates the requested article' do
+          patch api_article_url(another_article),
+                params: { article: another_user_article_new_attributes },
+                headers: another_user_valid_headers,
+                as: :json
+          another_article.reload
+          expect(another_article.title).to eq(another_user_article_new_attributes[:title])
+        end
       end
     end
 
@@ -138,16 +179,32 @@ RSpec.describe '/articles', type: :request do
   end
 
   describe 'DELETE /destroy' do
-    it 'destroys the requested article' do
-      article
-      expect do
-        delete api_article_url(article), headers: valid_headers, as: :json
-      end.to change(Article, :count).by(-1)
-    end
+    context 'with the first user' do
+      it 'destroys the requested article' do
+        article
+        expect do
+          delete api_article_url(article), headers: valid_headers, as: :json
+        end.to change(Article, :count).by(-1)
+      end
 
-    it_behaves_like 'user not logged in' do
-      let(:url) do
-        delete api_article_url(article), headers: {}, as: :json
+      it_behaves_like 'user not logged in' do
+        let(:url) do
+          delete api_article_url(article), headers: {}, as: :json
+        end
+      end
+    end
+    context 'with another user' do
+      it 'destroys the requested article' do
+        article_two
+        expect do
+          delete api_article_url(article_two), headers: another_user_valid_headers, as: :json
+        end.to change(Article, :count).by(-1)
+      end
+
+      it_behaves_like 'user not logged in' do
+        let(:url) do
+          delete api_article_url(article_two), headers: {}, as: :json
+        end
       end
     end
   end
